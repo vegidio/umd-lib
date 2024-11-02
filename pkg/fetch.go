80ff93a4e45396f2fd7e85fdd961a29188c059b6
Vinicius Egidio <me@vinicius.io>
2024-11-02 22:08:41 +0100

    feat(pkg): implement EpochTime type

---
+++ pkg/fetch.go
@@ -0,0 +1,80 @@
+package pkg
+
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"net/http"
+	"time"
+)
+
+type Fetch struct {
+	client *resty.Client
+}
+
+// region
+
+// NewFetch creates a new Fetch instance with specified headers and retry settings.
+//
+// Parameters:
+//   - headers: a map of headers to be set on each request.
+//   - retries: the number of retry attempts for failed requests.
+func NewFetch(headers map[string]string, retries int) Fetch {
+	return Fetch{
+		client: resty.New().
+			SetHeaders(headers).
+			SetRetryCount(retries).
+			SetRetryWaitTime(1 * time.Second).
+			SetRetryMaxWaitTime(60 * time.Second).
+			AddRetryCondition(
+				func(r *resty.Response, err error) bool {
+					return err != nil || r.StatusCode() == http.StatusTooManyRequests
+				},
+			),
+	}
+}
+
+// GetText performs a GET request to the specified URL and returns the response body as a string.
+//
+// Parameters:
+//   - url: the URL to send the GET request to.
+//
+// Returns the response body as a string and an error if the request fails.
+func (f Fetch) GetText(url string) (string, error) {
+	resp, err := f.client.R().
+		Get(url)
+
+	if err != nil {
+		return "", fmt.Errorf("fetch error - GetString - %v", err)
+	}
+
+	if resp.IsError() {
+		return "", fmt.Errorf("fetch error - GetString - %d", resp.StatusCode())
+	}
+
+	return resp.String(), nil
+}
+
+// DownloadFile performs a GET request to the specified URL and saves the response body to the specified file path.
+//
+// Parameters:
+//   - url: the URL to send the GET request to.
+//   - filePath: the path where the response body should be saved.
+//
+// Returns the size of the downloaded file and an error if the request fails.
+func (f Fetch) DownloadFile(url string, filePath string) (int64, error) {
+	resp, err := f.client.R().
+		SetOutput(filePath).
+		Get(url)
+
+	if err != nil {
+		return 0, fmt.Errorf("fetch error - DownloadFile - %v", err)
+	}
+
+	if resp.IsError() {
+		return 0, fmt.Errorf("fetch error - DownloadFile - %d", resp.StatusCode())
+	}
+
+	return resp.Size(), nil
+}
+
+// endregion
---
+++ pkg/fetch_test.go
@@ -0,0 +1,53 @@
+package pkg
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestFetch_GetText(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World!"))
+	}))
+
+	defer server.Close()
+
+	fetch := NewFetch(nil, 0)
+	body, err := fetch.GetText(server.URL)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "Hello, World!", body)
+}
+
+func TestFetch_GetText_Error(t *testing.T) {
+	fetch := NewFetch(nil, 0)
+	_, err := fetch.GetText("http://invalid-url")
+
+	assert.Error(t, err)
+}
+
+func TestFetch_DownloadFile(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("file content"))
+	}))
+
+	defer server.Close()
+
+	fetch := NewFetch(nil, 0)
+	filePath := "testfile.txt"
+	size, err := fetch.DownloadFile(server.URL, filePath)
+
+	assert.NoError(t, err)
+	assert.Equal(t, int64(len("file content")), size)
+}
+
+func TestFetch_DownloadFile_Error(t *testing.T) {
+	fetch := NewFetch(nil, 0)
+	_, err := fetch.DownloadFile("http://invalid-url", "testfile.txt")
+
+	assert.Error(t, err)
+}
