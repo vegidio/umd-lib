856253152109efc68b5e3129f07098d5a1abc69a
Vinicius Egidio <me@vinicius.io>
2025-03-22 19:16:26 +0100

    test: Update the Media tests

--- internal/model/media_test.go
+++ internal/model/media_test.go
@@ -6,56 +6,54 @@ import (
 )
 
 func TestMedia_UrlWithValidExtension(t *testing.T) {
-	media := Media{Url: "http://example.com/file.mp4"}
+	media := NewMedia("http://example.com/file.mp4", Reddit, nil)
 	assert.Equal(t, "mp4", media.Extension)
 }
 
 func TestMedia_UrlWithValidExtensionAndQueryString(t *testing.T) {
-	media := Media{Url: "http://example.com/file.mp4?query=1"}
+	media := NewMedia("http://example.com/file.mp4?query=1", Reddit, nil)
 	assert.Equal(t, "mp4", media.Extension)
 }
 
 func TestMedia_UrlWithNoExtension(t *testing.T) {
-	media := Media{Url: "http://example.com/file"}
+	media := NewMedia("http://example.com/file", Reddit, nil)
 	assert.Equal(t, "", media.Extension)
 }
 
 func TestMedia_EmptyUrl(t *testing.T) {
-	media := Media{Url: ""}
+	media := NewMedia("", Reddit, nil)
 	assert.Equal(t, "", media.Extension)
 }
 
 func TestMedia_InvalidUrl(t *testing.T) {
-	media := Media{Url: "://invalid-url"}
-	assert.Equal(t, "", media.Extension)
+	assert.Panics(t, func() {
+		_ = NewMedia("://invalid-url", Reddit, nil)
+	})
 }
 
 func TestMedia_TypeImage(t *testing.T) {
-	media := Media{Url: "http://example.com/image.jpg"}
+	media := NewMedia("http://example.com/image.jpg", Reddit, nil)
 	assert.Equal(t, Image, media.Type)
 
-	media = Media{Url: "http://example.com/image.avif"}
+	media = NewMedia("http://example.com/image.avif", Reddit, nil)
 	assert.Equal(t, Image, media.Type)
 
-	media = Media{Url: "http://example.com/image.png"}
+	media = NewMedia("http://example.com/image.png", Reddit, nil)
 	assert.Equal(t, Image, media.Type)
 }
 
 func TestMedia_TypeVideo(t *testing.T) {
-	media := Media{Url: "http://example.com/video.mp4"}
+	media := NewMedia("http://example.com/video.mp4", Reddit, nil)
 	assert.Equal(t, Video, media.Type)
 
-	media = Media{Url: "http://example.com/video.mkv"}
+	media = NewMedia("http://example.com/video.mkv", Reddit, nil)
 	assert.Equal(t, Video, media.Type)
 
-	media = Media{Url: "http://example.com/video.webm"}
+	media = NewMedia("http://example.com/video.webm", Reddit, nil)
 	assert.Equal(t, Video, media.Type)
 }
 
 func TestMedia_TypeUnknown(t *testing.T) {
-	media := Media{Url: "http://example.com/file.unknown"}
-	assert.Equal(t, Unknown, media.Type)
-
-	media = Media{Url: "http://example.com/file.unknown"}
+	media := NewMedia("http://example.com/file.unknown", Reddit, nil)
 	assert.Equal(t, Unknown, media.Type)
 }
